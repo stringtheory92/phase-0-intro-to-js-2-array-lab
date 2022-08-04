bda889c7e88550a399b470310983e32b02e15285
Adam Kabak <adamkabak@yahoo.com>
2022-08-04 12:23:09 -0400

    commit tree b8eedc7a4458be456b51f135418d90ef913abb24

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+    function destructivelyAppendCat(name) {
+        cats.length = 0;
+        cats.push("Milo", "Otis", "Garfield");
+        return cats.push(name);
+ }
+    function destructivelyPrependCat(name) {
+        cats.length = 0;
+        cats.push("Milo", "Otis", "Garfield");
+        return cats.unshift(name);
+    }
+    function destructivelyRemoveLastCat() {
+        cats.length = 0;
+        cats.push("Milo", "Otis", "Garfield");
+        return cats.pop();
+    }
+    function destructivelyRemoveFirstCat() {
+        cats.length = 0;
+        cats.push("Milo", "Otis", "Garfield");
+        return cats.shift();
+    }
+    function appendCat(name) {
+        cats.length = 0;
+        cats.push("Milo", "Otis", "Garfield");
+        return [...cats, name];
+    }
+    function prependCat(name) {
+        cats.length = 0;
+        cats.push("Milo", "Otis", "Garfield");
+        return [name, ...cats];
+    }
+    function removeLastCat() {
+        cats.length = 0;
+        cats.push("Milo", "Otis", "Garfield");
+        return cats.slice(0, -1);
+    }
+    function removeFirstCat() {
+        cats.length = 0;
+        cats.push("Milo", "Otis", "Garfield");
+        return cats.slice(1);
+    }
